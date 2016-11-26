98070c9a046df84c99abaa455ce2632bcb7b6885
Teruo Kunihiro <yuyuyu1999@gmail.com>
2016-11-26 21:23:21 +0900

    #1 Add Test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,20 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+import ReactDOM from 'react-dom';
+import TestUtils from 'react-addons-test-utils';
+import App from './App';
+import GameBoardStore from './stores/gameBoardStore'
+import BoardStore from './stores/boardStore'
+
+it('CheckboxWithLabel changes the text after click', () => {
+    const gameBoardStore = new GameBoardStore(new BoardStore())
+    // Render a checkbox with label in the document
+    const app = TestUtils.renderIntoDocument(
+        <App gameBoardStore={gameBoardStore}/>,
+    );
+
+    const appNode = ReactDOM.findDOMNode(app);
+
+    expect(appNode.textContent).toEqual('Game over!Try again');
 });
+
